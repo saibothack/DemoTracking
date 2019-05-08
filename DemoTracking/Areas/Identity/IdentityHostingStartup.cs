1185621542ba4e884c27993b254d3d38f11cff21
Gad Arenas <garenas.grupoempresarial322@gmail.com>
2019-05-08 13:16:06 -0500

    Commit inicial

---
+++ DemoTracking/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DemoTracking.Areas.Identity.Data;
+using DemoTracking.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DemoTracking.Areas.Identity.IdentityHostingStartup))]
+namespace DemoTracking.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DemoTrackingContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DemoTrackingContextConnection")));
+
+                services.AddDefaultIdentity<User>()
+                    .AddEntityFrameworkStores<DemoTrackingContext>();
+            });
+        }
+    }
+}
---
+++ DemoTracking/Models/Company.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoTracking.Models
+{
+    public class Company
+    {
+        public string id { get; set; }
+        public string Name { get; set; }
+        public string RFC { get; set; }
+        public CompanyStatus Status { get; set; }
+    }
+
+    public enum CompanyStatus {
+        Activo,
+        Inactivo
+    }
+}
